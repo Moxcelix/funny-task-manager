265c2c35e275c5754a7ba0e63ed78212519c6715
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-26 16:01:06 +0300

    Create TaskManager.cs

---
+++ src/Models/TaskManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManager.Models
+{
+    /// <summary>
+    /// Класс диспетчера задач.
+    /// </summary>
+    public class TaskManager
+    {
+        /// <summary>
+        /// Список задач.
+        /// </summary>
+        private readonly List<Task> _tasks = new List<Task>();
+
+        /// <summary>
+        /// Метод добавления задачи.
+        /// </summary>
+        /// <param name="task"></param>
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+
+        /// <summary>
+        /// Метод обновления (один такт).
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+}
