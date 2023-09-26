1a9f6a2a44a87f65cdefa398efda0bbe7ce4d620
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-26 12:35:09 +0300

    Create TaskState.cs

---
+++ src/Models/TaskState.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Models
+{
+    /// <summary>
+    /// Перечисление состояний задачи.
+    /// </summary>
+    public enum TaskState
+    {
+        Running,    // Выполнение.
+        Sleeping,   // Бездействие.
+        Ready,      // Готовность.
+        Output      // Ожидание окончания вывода.
+    }
+}
